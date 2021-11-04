2c14125c279669900fda59d90e1bc1b1260b1d12
Damian Zbrożek <damianzbrozek@hotmail.com>
2021-11-04 10:19:27 +0100

    fix: send report as separate emails

--- emenuapi/menus/tasks.py
+++ emenuapi/menus/tasks.py
@@ -3,7 +3,7 @@ from typing import List, cast
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
+from django.core.mail import send_mass_mail
 from django.template.loader import render_to_string
 from django.utils import timezone
 from menus.models import Dish
@@ -11,33 +11,30 @@ from menus.models import Dish
 from emenuapi.celery import app
 
 
-def send_dish_report() -> bool:
+def send_dish_report() -> int:
     yesterday = (timezone.now() - datetime.timedelta(days=1)).date()
 
     new_dishes = Dish.objects.filter(created__date=yesterday)
     updated_dishes = Dish.objects.filter(updated__date=yesterday)
 
     if not new_dishes.exists() and not updated_dishes.exists():
-        return False
+        return 0
 
-    emails = cast(
-        List[str], list(User.objects.filter(is_active=True).exclude(email='').values_list('email', flat=True))
+    recipient_list = cast(
+        List[str],
+        list(User.objects.filter(is_active=True).exclude(email='').order_by('id').values_list('email', flat=True)),
     )
 
-    if not emails:
-        return False
+    if not recipient_list:
+        return 0
+
+    if not settings.FROM_EMAIL:
+        return 0
 
     message = render_to_string('emails/dish_report.txt', {'new_dishes': new_dishes, 'updated_dishes': updated_dishes})
+    messages = [('Daily Dish Report', message, settings.FROM_EMAIL, [recipient]) for recipient in recipient_list]
 
-    return bool(
-        send_mail(
-            'Daily Dish Report',
-            message,
-            settings.FROM_EMAIL,
-            emails,
-            fail_silently=False,
-        )
-    )
+    return send_mass_mail(messages)
 
 
 @app.task
